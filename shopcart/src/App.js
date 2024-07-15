dbffd9b1e1a649e2cabd2538de854bab1993fc8a
Ann Maria Jose <annmariajose1412@gmail.com>
2024-07-15 06:04:33 -0500

    Update header logo

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,4 +1,5 @@
 import DisplayProducts from './DisplayProducts';
+import './App.css';
 import React, { useState } from 'react';
 import { ProductData } from "./Products";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -35,9 +36,15 @@ function App() {
 
   return (
     <div className="App">
-      <header className="sticky-top p-5 py-4 bg-info text-black d-flex flex-row justify-content-between align-items-center">
-        <h2>Shop to React</h2>
-        <div className='d-inline-flex align-items-center'>
+      <header className="sticky-top p-4 py-4 bg-info text-white d-flex flex-row justify-content-between align-items-center">
+        <button className='logoButton text-white bg-info'>
+          <h2>Shop 2&nbsp;
+            <span className='logoR border rounded-circle bg-white text-info'>
+              &nbsp;R&nbsp;
+            </span>
+          eact</h2>
+        </button>
+        <div className='d-inline-flex align-items-center px-2'>
           <FontAwesomeIcon className='m-3' icon={faShoppingCart} />
           <p className='mt-3'>{cartCount} items</p>
         </div>
